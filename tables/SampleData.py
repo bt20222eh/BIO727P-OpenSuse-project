6793deba8680e0e84b08138a86a7521028fc2ed4
hannaabby <149078786+hannaabby@users.noreply.github.com>
2024-02-20 14:31:15 +0000

    Update SampleData.py

--- tables/SampleData.py
+++ tables/SampleData.py
@@ -1,6 +1,7 @@
 # making tsv with only sample/snp id data
-# Extract SNP ID and Sample ID
-snp_sample_df = variant_data_df[['Sample_ID', 'SNP_ID']]
+# Extract SNP ID and Sample ID - table neccessary to match every  SAMPLE_ID to the matching SNP_ID
+output_df = pd.read_csv('output.tsv' , sep='\t')
+snp_sample_df = output_df[['Sample_ID', 'SNP_ID']]
 
 # Drop duplicates if necessary
 snp_sample_df = snp_sample_df.drop_duplicates()
