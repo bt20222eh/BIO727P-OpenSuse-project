710f437bf3cf2d0c0d5d42be4076756a6dd85ea9
bt20222eh <148054564+bt20222eh@users.noreply.github.com>
2024-02-27 23:21:15 +0000

    Update SamplePopulation.py

--- tables/SamplePopulation.py
+++ tables/SamplePopulation.py
@@ -124,6 +124,30 @@ combined_freq_file = ('combined_frequencies.tsv')
 print(f"Combined data has been written to {combined_file}.")
 
 import pandas as pd
+# Load the dataset
+file_path_population = 'sample_pop.tsv'
+population_df = pd.read_csv(file_path_population, sep='\t', header=0)
+
+# Define the mapping from population codes to super-populations
+super_population_mapping = {
+    'ACB': 'AFR', 'ASW': 'AFR', 'ESN': 'AFR', 'GWD': 'AFR', 'LWK': 'AFR', 'MSL': 'AFR', 'YRI': 'AFR',
+    'CLM': 'AMR', 'MXL': 'AMR', 'PEL': 'AMR', 'PUR': 'AMR',
+    'CDX': 'EAS', 'CHB': 'EAS', 'CHS': 'EAS', 'JPT': 'EAS', 'KHV': 'EAS',
+    'CEU': 'EUR', 'FIN': 'EUR', 'GBR': 'EUR', 'IBS': 'EUR', 'TSI': 'EUR',
+    'BEB': 'SAS', 'GIH': 'SAS', 'ITU': 'SAS', 'PJL': 'SAS', 'STU': 'SAS', 'SIB' :'EAS'
+}
+
+# Map population codes to super-populations, leaving 'SIB' or any other undefined codes blank
+population_df['Super-population'] = population_df['population'].map(super_population_mapping)
+
+# Verify the changes
+print(population_df.head())
+unique=population_df.drop_duplicates()
+
+# Display the DataFrame after removing duplicates to verify
+unique
+# save the dataframe into a tsv file. 
+unique.to_csv('population_superpopualtion.tsv', sep='\t', index=False)
 
 # load the relevant TSV files
 combined_freq_file_path = 'combined_frequencies.tsv'
